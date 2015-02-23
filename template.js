514f231f2add31bec74566d27074912fc22fd6e6
dennyriadi <denny.riadi@findly.com>
2015-02-23 15:00:00 +1300

    Adding back in licenses attribute in package.json

--- template.js
+++ template.js
@@ -24,6 +24,7 @@ exports.template = function(grunt, init, done) {
     init.prompt('repository'),
     init.prompt('homepage'),
     init.prompt('bugs'),
+    init.prompt('licenses', 'Findly'),
     init.prompt('author_name'),
     init.prompt('author_email'),
     init.prompt('author_url'),
